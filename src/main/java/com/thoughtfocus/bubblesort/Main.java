a4b09998953224c86081ff241a3c43153a809a30
106580 <saisunilkm>
2022-02-25 11:15:05 +0530

    BubbleSort

---
+++ src/main/java/com/thoughtfocus/bubblesort/Main.java
@@ -0,0 +1,13 @@
+package com.thoughtfocus.bubblesort;
+
+public class Main {
+    public static void main(String args[])
+    {
+        BubbleSort ob = new BubbleSort();
+        int arr[] = {60,87,55, 1, 14, 110, 90};
+        ob.bubbleSort(arr);
+        System.out.println("Sorted array");
+        ob.printArray(arr);
+    }
+
+}
